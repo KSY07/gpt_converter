a45017566f8ede139191068540da28de87fa7ba7
김세영 <ksy2008w@naver.com>
2024-08-30 01:49:55 +0900

    Init

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import httpx
+from openai import OpenAI
+import os
+
+OPENAI_API_KEY = "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"
+app = FastAPI()
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+
+class GPTRequest(BaseModel):
+    prompt: str
+@app.post("/gpt/request")
+async def get_gpt_response(request:GPTRequest):
+    if not OPENAI_API_KEY:
+        raise HTTPException(status_code=500, detail="OPENAI_API_KEY is missing")
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini-2024-07-18",
+        messages=[
+            {"role": "system", "content": "너는 지금부터 캐드 도면을 그려줘야해."},
+            {"role": "user", "content": request.prompt},
+        ]
+    )
+
+    return {"gpt_response": completion.choices[0].message}
