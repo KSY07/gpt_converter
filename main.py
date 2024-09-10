e6131e45ad829baa87e76f4907424c351a4b0318
tandoori <ksy2008w@naver.com>
2024-09-10 14:39:17 +0900

    2024-09-10 - 커트라인

--- main.py
+++ main.py
@@ -25,3 +25,18 @@ async def get_gpt_response(request:GPTRequest):
     )
 
     return {"gpt_response": completion.choices[0].message}
+
+@app.post("/gpt/request/image")
+async def get_gpt_image_response(request:GPTRequest):
+    if not OPENAI_API_KEY:
+        raise HTTPException(status_code=500, detail="OPENAI_API_KEY is missing")
+
+    completion = client.image.generate(
+        model="dall-e-3",
+        prompt=request.prompt,
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+
+    return {"image_response": completion.data[0].url}
