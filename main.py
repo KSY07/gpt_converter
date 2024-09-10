4ff4a15e043d43496c9536bfb4801b857d8a2c6f
tandoori <ksy2008w@naver.com>
2024-09-10 14:46:14 +0900

    2024-09-10 - CORS 요청 추가

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import httpx
 from openai import OpenAI
 import os
 
+from starlette.middleware.cors import CORSMiddleware
+
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 app = FastAPI()
 client = OpenAI(api_key=OPENAI_API_KEY)
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class GPTRequest(BaseModel):
